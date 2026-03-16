798bc38ea5729a6ae5bbb284bb5643a5f711a8ab
laacin <lautarocingolani07@gmail.com>
2026-03-16 04:42:16 -0300

    feat(app): add `otpLogin` method in AuthUseCase

--- src/app/use-cases/auth.use-case.ts
+++ src/app/use-cases/auth.use-case.ts
@@ -1,59 +1,116 @@
+import { TwoFactorService } from '@app/services/2fa.service';
+import { CachingService } from '@app/services/caching.service.js';
 import { TokenService } from '@app/services/token.service';
 import { ROLE, User } from '@domain/entities/user.entity';
 import type { UserRepo } from '@domain/repos/user.repo';
 import { USER_REPO } from '@domain/tokens';
 import { Inject, Injectable } from '@nestjs/common';
 import { hash, verify } from 'argon2';
-
-// Params
-interface RegisterParams {
-  email: string;
-  password: string;
-  username: string;
-}
-
-interface LoginParams {
-  id?: string;
-  email?: string;
-  username?: string;
-  password: string;
-}
+import type {
+  OtpLoginIn,
+  OtpLoginOut,
+  RegisterIn,
+  RegisterOut,
+  StdLoginIn,
+  StdLoginOut,
+} from './auth.use-case.types.ts';
 
 @Injectable()
 export class AuthUseCase {
   constructor(
     @Inject(USER_REPO) private readonly userRepo: UserRepo,
     @Inject() private readonly token: TokenService,
+    @Inject() private readonly otp: TwoFactorService,
+    @Inject() private readonly cache: CachingService,
   ) {}
 
-  async register({ email, password, username }: RegisterParams) {
-    if (!(await this.userRepo.exists({ email, username }))) {
+  async register({
+    email,
+    password,
+    username,
+  }: RegisterIn): Promise<RegisterOut> {
+    if (await this.userRepo.exists({ email, username })) {
       throw new Error('user already exists');
     }
 
     const password_hash = await hash(password);
-    const id = crypto.randomUUID() as string;
+    const userId = crypto.randomUUID() as string;
 
     const user: User = {
-      id,
+      id: userId,
       email,
       password: password_hash,
       username,
       createdAt: new Date(),
       role: ROLE.CLIENT,
+      trustedDevices: [],
     };
 
     await this.userRepo.store(user);
-    return { id, email, username };
+    return { userId };
   }
 
-  async login({ id, email, username, password }: LoginParams) {
-    const user = await this.userRepo.get({ id, email, username });
+  async stdLogin({
+    deviceId,
+    email,
+    username,
+    password,
+  }: StdLoginIn): Promise<StdLoginOut> {
+    const user = await this.userRepo.get({ email, username });
     if (!user || !(await verify(user.password, password))) {
       throw new Error('invalid user or password');
     }
 
+    if (this.isRequired2fa({ user, deviceId })) {
+      await this.cache.setOtpPending(user.id, 15000);
+      throw new Error('require 2FA');
+    }
+
     const [access, refresh] = await this.token.create(user);
     return { access, refresh };
+  }
+
+  async otpLogin({
+    userId,
+    otpCode,
+    saveDeviceId,
+    deviceId,
+  }: OtpLoginIn): Promise<OtpLoginOut> {
+    if (!(await this.cache.isOtpPending(userId))) {
+      throw new Error('unexpected 2FA login');
+    }
+
+    const user = await this.userRepo.get({ id: userId });
+    if (!user) throw new Error('invalid user');
+    if (!user.otpSecret) throw new Error('2FA is not active');
+    const { otpSecret } = user;
+
+    if (!(await this.otp.isValid({ otpSecret, otpCode }))) {
+      throw new Error('Invalid 2FA code');
+    }
+
+    if (saveDeviceId) {
+      await this.userRepo.update(user.id, {
+        trustedDevices: [...user.trustedDevices, deviceId],
+      });
+    }
+
+    await this.cache.setOtpDone(userId);
+    const [access, refresh] = await this.token.create(user);
+    return { access, refresh };
+  }
+
+  // internal use-cases
+  isRequired2fa({
+    user,
+    deviceId,
+  }: {
+    user: User;
+    deviceId?: string;
+  }): boolean {
+    if (!user.otpSecret) return false;
+    if (!deviceId) return true;
+
+    return !user.trustedDevices.includes(deviceId);
   }
 }
